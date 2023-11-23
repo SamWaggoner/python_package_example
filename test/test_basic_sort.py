01189a9fe3f03667f280f65acb879d074645984f
Sam Waggoner <samwags3@gmail.com>
2023-11-22 22:53:58 -0500

    fixed negative testing

--- test/test_basic_sort.py
+++ test/test_basic_sort.py
@@ -26,13 +26,8 @@ def is_sorted(int_list):
     """
     return int_list == sorted(int_list)
 
-
 @pytest.fixture
 def int_lists():
-    '''
-    List of lists to run as pytest fixtures, testing the sorting algorithms we 
-    designed
-    '''
     return [
         # Sorted list
         [1, 2, 3],
@@ -47,48 +42,50 @@ def int_lists():
         # Empty list
         [],
 
-        # # TODO: THIS EXAMPLE FAILS BUT SHOULDN'T
-        # # Random list generated using NumPy
-        # list(np.random.randint(low=-10, high=200, size=5)),
+        # Random list generated using NumPy
+        list(np.random.randint(low=-10, high=200, size=5)),
 
-        # # TODO: BELOW, WRITE TEST INPUTS THAT SHOULD FAIL, AND ENSURE THEY DO FAIL
-        # # String input
-        # "hi I am a string!",
-        # # Single char input
-        # "q",
-        # # Empty string input
-        # "",
-        # # Decimal input
-        # [0.2, 5.4234156, 0.000000001, -99.9],
-        # # Mixed input
-        # [6, 2, "oop", 74],
-        # # 2D lists
-        # [[8,5,2],[82,3]],
-        # # 2D empty lists
-        # [[]],
-        # [[],[]]
-
+        # String input
+        "hi I am a string!",
+        # Single char input
+        "q",
+        # Empty string input
+        "",
+        # Decimal input
+        [0.2, 5.4234156, 0.000000001, -99.9],
+        # Mixed input
+        [6, 2, "oop", 74],
+        # 2D lists
+        [[8,5,2],[82,3]],
+        # 2D empty lists
+        [[]],
+        [[],[]]
     ]
 
-'''
-For each sorting algorithm we will test all of the pytest fixtures
-'''
-# Test the bubble sort algorithm
+# Test each of the three implementations. If an exception is raised, ensure
+# that the test passes if either 1) the input is not a list, or 2) not every
+# element in that list is an integer.
+
 def test_bubble(int_lists):
     for example in int_lists:
-        sorted_list = bubble(example.copy())
-        assert is_sorted(sorted_list)
+        try:
+            sorted_list = bubble(example.copy())
+            assert is_sorted(sorted_list)
+        except Exception:
+            assert isinstance(example, list) == False or all(isinstance(i, int) for i in example) == False
 
-
-# Test the quick sort algorithm
 def test_quick(int_lists):
     for example in int_lists:
-        sorted_list = quick(example.copy())
-        assert is_sorted(sorted_list)
+        try:
+            sorted_list = quick(example.copy())
+            assert is_sorted(sorted_list)
+        except Exception:
+            assert isinstance(example, list) == False or all(isinstance(i, int) for i in example) == False
 
-
-# Test the insertion sort algorithm
 def test_insertion(int_lists):
     for example in int_lists:
-        sorted_list = insertion(example.copy())
-        assert is_sorted(sorted_list)
+        try:
+            sorted_list = insertion(example.copy())
+            assert is_sorted(sorted_list)
+        except Exception:
+            assert isinstance(example, list) == False or all(isinstance(i, int) for i in example) == False
